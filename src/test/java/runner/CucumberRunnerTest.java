438be76b21a81af063fce8f795f3ac1d04b20ffa
jata2011 <jata2011@gmail.com>
2024-02-29 12:36:06 +1100

    Added configuration in pom.xml to run from command line

--- src/test/java/runner/CucumberRunnerTest.java
+++ src/test/java/runner/CucumberRunnerTest.java
@@ -1,28 +1,21 @@
 package runner;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.vimalselvam.cucumber.listener.Reporter;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-
-import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/feature",
         glue={"steps"},
         plugin = {"summary", "pretty", "html:target/cucumber-reports.html",
-                "json:target/cucumber-reports",
+                "json:target/cucumber-reports/Report.json", //report.json is to get cucumber report in azure devops
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
 
         monochrome = true
 )
 
-public class CucumberRunnerTests {
+public class CucumberRunnerTest {
 
 }
