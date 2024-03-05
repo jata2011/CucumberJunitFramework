5874efd42f2ec79f4fb4349cc25cc490ecef4025
jata2011 <jata2011@gmail.com>
2024-03-05 19:52:32 +1100

    Commit

--- src/test/java/dataProviders/ConfigFileReader.java
+++ src/test/java/dataProviders/ConfigFileReader.java
@@ -2,16 +2,27 @@ package dataProviders;
 
 import java.io.*;
 import java.util.Properties;
+import dataProviders.ConfigFileReader;
+import utils.HelperClass;
 
-import static utils.HelperClass.environmentFromYAML;
+import static utils.HelperClass.logger;
+import static utils.HelperClass.varenvt;
+
 
 public class ConfigFileReader {
+
+    public ConfigFileReader(){
+        varenvt = System.getenv("Environment");
+        logger.info("env Value: " + varenvt);
+
+    }
 
     private static Properties properties;
 
 
+
     static {
-          try (InputStream input = new FileInputStream("src/test/resources/config-"+ "test" + ".properties")) {
+          try (InputStream input = new FileInputStream("src/test/resources/config-"+ varenvt + ".properties")) {
             properties = new Properties();
             properties.load(input);
         } catch (IOException e) {
